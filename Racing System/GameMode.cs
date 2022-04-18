207baf6a0f5e93515a1575f9f9bece68305c4f30
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-04-17 19:26:00 -0500

    Agregar archivos de proyecto.

---
+++ Racing System/GameMode.cs
@@ -0,0 +1,21 @@
+﻿using SampSharp.GameMode;
+using System;
+
+namespace Racing_System
+{
+    public class GameMode : BaseMode
+    {
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            Console.WriteLine("\n----------------------------------");
+            Console.WriteLine(" Blank game mode by your name here");
+            Console.WriteLine("----------------------------------\n");
+
+            SetGameModeText("Blank game mode");
+
+            // TODO: Put logic to initialize your game mode here
+        }
+    }
+}
---
+++ Racing System/Player.cs
@@ -0,0 +1,17 @@
+﻿using SampSharp.GameMode.Pools;
+using SampSharp.GameMode.World;
+using System;
+
+namespace Racing_System
+{
+    [PooledType]
+    public class Player : BasePlayer
+    {
+        public override void OnConnected(EventArgs e)
+        {
+            base.OnConnected(e);
+
+            SendClientMessage("Welcome to blank game mode by your name here!");
+        }
+    }
+}
