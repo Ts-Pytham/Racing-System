1523e92165d7d01af2815c859c37f5a6e482ce9b
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-04-18 15:08:38 -0500

    Fix GetCar Command

--- Racing System/src/Command/Commands.cs
+++ Racing System/src/Command/Commands.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SampSharp.GameMode.Definitions;
 
 namespace Racing_System.Command
 {
@@ -14,15 +15,31 @@ namespace Racing_System.Command
     public partial class Commands
     {
         [Command("car", Shortcut ="car", UsageMessage = "/car [idcar] [colour1] [colour2]")]
-        private static void GetCar(Player player, int idcar, int colour1, int colour2)
+        private static void GetCar(Player player,int idcar, int colour1, int colour2)
         {
+
             if (idcar < 0 || colour1 < 0 || colour2 < 0)
+            {
+                player.SendClientMessage("El id/color1/color2 no puede ser negativo!");
+                return;
+            }
+            if(!(idcar >= 400 && idcar <= 611))
+            {
+                player.SendClientMessage("El id del vehículo no está en el rango [400-611]!");
+                return;
+            }
+   
+           BaseVehicle.Create((VehicleModelType)idcar, player.Position, 90, colour1, colour2);
+
+        }
+
+        [Command("vw", Shortcut = "vw", UsageMessage = "/vw [idVirtualWorld]")]
+        private static void GetCar(Player player, int vw)
+        {
+            if (vw < 0 )
                 player.SendClientMessage("El id no puede ser negativo!");
-            
-            BaseVehicle vehicle = new ();
-            vehicle.ChangeColor(colour1, colour2);
-            vehicle.Position = player.Position;
-            
+            player.VirtualWorld = vw;
+            player.SendClientMessage($"Te has cambiado al Virtual World: {vw}");
         }
     }
 }
